5ab035b18ddce67a3608c46ac52f9c9411deb7b9
Octopus314 <mikezhang_11@hotmail.com>
2024-07-01 17:14:30 +0800

    feat: server to group

---
+++ octopus/plugins/from_server.py
@@ -0,0 +1,24 @@
+import os
+from config import *
+import nonebot
+
+try:
+    logFile = open(os.path.join(SERVER_ROOT, 'logs/latest.log'))
+    logFile.seek(0, 2)
+except FileNotFoundError:
+    nonebot.log.logger.error('Unable to open log file, maybe server not started? ')
+    if not DEBUG:
+        exit(0)
+
+@nonebot.scheduler.scheduled_job('interval', seconds = 5)
+async def _():
+    bot = nonebot.get_bot()
+    while True:
+        line = logFile.readline()
+        if not line:
+            break
+        line = line.split(LOG_IDENTIFIER, 1)
+        if len(line) == 1:
+            continue
+        print(line[1])
+        await nonebot.get_bot().send_group_msg(group_id = GROUP_ID, message = line[1])
