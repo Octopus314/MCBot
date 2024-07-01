104182f3869d6f8898ab87b43a43394d76971697
Octopus314 <mikezhang_11@hotmail.com>
2024-07-01 13:57:22 +0800

    add config template

---
+++ config_template.py
@@ -0,0 +1,14 @@
+from nonebot.default_config import *
+
+DEBUG = True
+SUPERUSERS = {}
+COMMAND_START = {''}
+COMMAND_SEP = {}
+HOST = '127.0.0.1'
+POST = 8080
+RCON_HOST = '127.0.0.1'
+RCON_PORT = 25575
+RCON_PASSWD = ''
+GROUP_ID = 11111111
+PLAYERNAME_MAP_PATH = './names.json'
+SERVER_ROOT = '../GTNH/'
