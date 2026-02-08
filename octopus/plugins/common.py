4ce10dc898a79364dd3c28c49c5ec28c162c3e44
Octopus314 <mikezhang_11@hotmail.com>
2026-02-08 23:24:01 +0800

    feat: group card

---
+++ octopus/plugins/common.py
@@ -0,0 +1,17 @@
+from config import *
+import nonebot
+import aiocqhttp
+
+bot = nonebot.get_bot()
+
+@nonebot.scheduler.scheduled_job('cron', minute='*')
+async def _():
+    await bot.get_group_member_info(group_id=GROUP_ID, user_id=SELF_ID, no_cache=True)
+
+@nonebot.on_notice
+async def _(event: nonebot.NoticeSession):
+    event = event.event
+    if event['group_id'] != GROUP_ID or event['notice_type'] != 'group_card' or event['card_new'] == '':
+        return
+    await bot.send_group_msg(message = f"谁给我改的{event['card_new']}", group_id = GROUP_ID)
+    await bot.set_group_card(group_id = GROUP_ID, user_id = SELF_ID, card = '')
